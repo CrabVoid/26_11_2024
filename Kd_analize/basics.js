c62665502d39686cf262b0caf0de09364b12bac7
crab_void <you@example.com>
2024-12-03 08:08:30 +0200

    03_12_2024

---
+++ Kd_analize/basics.js
@@ -0,0 +1,22 @@
+/*let isStudent = false;
+
+if (isStudent == true) {
+    console.log("Students!");
+} else {
+    console.log("Nav students!");
+}
+*/
+
+/*let numArray = [51,2,12,4,22]
+
+for (i = 0; i < numArray.length; i++) {
+    console.log(i+1 + ". Skaitlis ir " + numArray[i]);
+}
+*/
+
+//function printErrorStatus(code, text) {
+  //  return `${code} - ${text}`;
+//}
+
+//console.log(printErrorStatus(502, "Bad Gateway"));
+//console.log(printErrorStatus(404, "Lapa nav atrasta"));
